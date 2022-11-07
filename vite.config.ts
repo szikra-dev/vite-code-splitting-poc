5ddf45a87419cf1bd75419a9e8a8a8dc57f6999d
David Arcís Moreno <szikra.dev@gmail.com>
2022-11-07 17:35:59 +0100

    lol

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
+import { dependencies } from './package.json'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import macrosPlugin from 'vite-plugin-babel-macros'
 
+const reactDeps = Object.keys(dependencies).filter(
+  (key) => key === 'react' || key.startsWith('react-'),
+)
+
+const manualChunks = {
+  vendor: reactDeps,
+  ...Object.keys(dependencies).reduce((chunks, name) => {
+    if (!reactDeps.includes(name)) {
+      chunks[name] = [name]
+    }
+    return chunks
+  }, {}),
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), macrosPlugin()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks,
+      },
+    },
+  },
 })
